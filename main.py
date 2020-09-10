a9d24b135d4f03b99fe3cca10eb8dc792002bc94
Sultanbek Jenisbekov <70880353+Sulaaa@users.noreply.github.com>
2020-09-10 16:41:05 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Drive--Y-- Link
+myName: str = "Sultanbek Jenisbekov"
+
+age: str = "17"
+
+placeofbirth: str = 'Atyraucity'
+
+groupe = "AutomatizationandControl"
+
+"/myName,mySurname,age, placeofbirth ,groupe"
+
+print ("Имя : ",myName,"Возраст :",age,"Из :",placeofbirth,"Учится в группе :",groupe)
